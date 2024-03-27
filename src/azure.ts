3cfe933b08bd87b3166e2e69a3a55cd7b0417f29
Tom Santens <tom.santens@gmail.com>
2024-03-27 17:47:12 +0100

    Azure: usable code examples from GitHub

---
+++ src/azure.ts
@@ -0,0 +1,10 @@
+/*
+ *  Azure
+ *  We're using Azure for our first cloud connection.
+ * 
+ *  Copyright  Sanne 'SpuQ' Santens
+ * 
+ *  Azure SDK examples:
+ *      https://github.com/Azure/azure-iot-sdk-node/tree/main/device/samples/typescript
+ *      https://github.com/Azure/azure-iot-sdk-node/tree/main/provisioning/device/samples
+ */
