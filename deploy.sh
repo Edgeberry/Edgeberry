d8a59b7de0e5b27f3edbf045c61a6e1fc5e6e175
Tom Santens <tom.santens@gmail.com>
2024-03-27 17:32:50 +0100

    Deploy script: default values in variables

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,11 @@
 
 #   deploy.sh
 #   Deploy the Edge Gateway application to a device on your
-#   local network using SSH.
+#   local network using sshpass.
+
+DEFAULT_USER=spuq
+DEFAULT_PASSWD=32bjfewAQZpd80x
+DEFAULT_HOST=192.168.1.105
 
 clear;
 
@@ -16,18 +20,20 @@ fi
 echo 'Remote access'
 read -p "Host: " HOST
 if [[ -z "$HOST" ]]; then
-    HOST=192.168.1.103
+    HOST=$DEFAULT_HOST
 fi
 
 read -p "User: " USER
 if [[ -z "$USER" ]]; then
-    USER=spuq
+    USER=$DEFAULT_USER
 fi
 
+stty -echo
 read -p "Password: " PASSWORD
 if [[ -z "$PASSWORD" ]]; then
-    PASSWORD=32bjfewAQZpd80x
+    PASSWORD=$DEFAULT_PASSWD
 fi
+stty -echo
 
 # Create a directory on the device for copying the project to
 echo "Creating a directory for copying the project to"
@@ -35,19 +41,22 @@ sshpass -p ${PASSWORD} ssh ${USER}@${HOST} "mkdir ~/temp"
 
 
 # Copy project to the device
-sshpass -p ${PASSWORD} scp -r ./* ${USER}@${HOST}:temp/
+sshpass -p ${PASSWORD} scp -r ./src ./package.json ./tsconfig.json ${USER}@${HOST}:temp/
 
-# Todo: installation steps on remote device (as root)
+# Install the application on remote device
 sshpass -p ${PASSWORD} ssh -o StrictHostKeyChecking=no ${USER}@${HOST} << EOF
 
 sudo su
 mkdir /opt/Edge_Gateway
 
 mv ./temp/* /opt/Edge_Gateway
+#rm -rf ./temp
 
 cd /opt/Edge_Gateway
 npm install --dev
 npm run build
+
+ps -ef | grep "pm2: Edge_Gateway" | grep -v grep
 
 EOF
 
