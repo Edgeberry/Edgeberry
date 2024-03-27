b3d8877e24c997bb3a9f666302d2f1a8cbee6539
Tom Santens <tom.santens@gmail.com>
2024-03-27 16:18:26 +0100

    Express: serve static HTML file from public folder

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.static( __dirname+'/public'));
 
 app.get('/', (req:any, res:any)=>{
-    return res.send('Hello World!');
+    return res.sendFile('index.html');
 });
 
 app.listen( 8080, ()=>{ console.log('Edge Gateway UI server running on port 8080')})
