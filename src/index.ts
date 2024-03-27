aae79a106da6e3aa6df735a06c8425d559d51ece
Tom Santens <tom.santens@gmail.com>
2024-03-27 16:11:27 +0100

    Express webserver concept

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello World!");
+/*
+ *  Edge Gateway
+ */
+
+const express = require('express');
+
+const app = express();
+
+app.use(express.static( __dirname+'/public'));
+
+app.get('/', (req:any, res:any)=>{
+    return res.send('Hello World!');
+});
+
+app.listen( 8080, ()=>{ console.log('Edge Gateway UI server running on port 8080')})
