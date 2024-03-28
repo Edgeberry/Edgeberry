b4bf78350d08bd0a016f2b5f8139d856604786d0
Tom Santens <tom.santens@gmail.com>
2024-03-28 10:32:22 +0100

    Connectivity API: Send Message implementation

--- src/routes/connectivity.ts
+++ src/routes/connectivity.ts
@@ -27,4 +27,18 @@ router.post('/azure/connectionparameters', (req:any, res:any)=>{
     }
 });
 
+/* Send a message to Azure IoT Hub */
+router.post('/azure/sendmessage', async(req:any, res:any)=>{
+    // Check the required parameters
+    if( typeof(req.body.message) !== 'string' || typeof(req.body.properties) === 'undefined')
+    return res.status(401).send({message:'Data incomplete'});
+    // Send the message
+    try{
+        await cloud.sendMessage( req.body.message );
+        return res.send({message:'Message sent'})
+    } catch(err){
+        return res.status(500).send({message:err});
+    }
+});
+
 export default router;
