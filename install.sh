da8cc00c46c4fde98ddbb8242e7844c3e274d76b
Tom Santens <tom.santens@gmail.com>
2024-04-07 10:42:44 +0200

    Installer improvements & styling

--- install.sh
+++ install.sh
@@ -13,6 +13,8 @@ REPONAME=EdgeBerry
 REPOOWNER=SpuQ
 ACCESSTOKEN=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
 
+# Start a clean screen
+clear;
 
 # Check if this script is running as root. If not, notify the user
 # to run this script again as root and cancel the installtion process
@@ -22,58 +24,96 @@ if [ "$EUID" -ne 0 ]; then
     exit 1;
 fi
 
+echo -e "Starting the \033[1m${APPNAME}\033[0m installation process..."
+echo -e "Some steps can take a while, but just have patience.\033[0m"
+echo ""
+
 # Check for NodeJS. If it's not installed, install it.
-echo -e "\e[0mChecking for NodeJS...\e[0m"
+echo -n -e "\e[0mChecking for NodeJS \e[0m"
 if which node >/dev/null 2>&1; then 
-    echo -e "\e[0;32mNodeJS is installed \e[0m"; 
+    echo -e "\e[0;32mInstalled \e[0m";
 else 
-    echo -e "\e[0;33mNodeJS is not installed \e[0m";
-    echo -e "\e[0mInstalling Node using apt \e[0m";
+    echo -e "\e[0;33mNot installed \e[0m";
+    echo -n -e "\e[0mInstalling Node using apt \e[0m";
     apt install -y node > /dev/null 2>&1;
+    # Check if the last command succeeded
+    if [ $? -eq 0 ]; then
+        echo -e "\e[0;32mSuccess!\e[0m"
+    else
+        echo -e "\e[0;33mFailed! Exit.\e[0m";
+        exit 1;
+    fi
 fi
 
 # Check for NPM. If it's not installed, install it.
-echo -e "\e[0mChecking for Node Package Manager (NPM)...\e[0m"
+echo -n -e "\e[0mChecking for Node Package Manager (NPM) \e[0m"
 if which npm >/dev/null 2>&1; then 
-    echo -e "\e[0;32mNPM is installed \e[0m"; 
+    echo -e "\e[0;32mInstalled \e[0m"; 
 else 
-    echo -e "\e[0;33mNPM is not installed \e[0m";
-    echo -e "\e[0mInstalling NPM using apt \e[0m";
+    echo -e "\e[0;33mNot installed \e[0m";
+    echo -n -e "\e[0mInstalling NPM using apt \e[0m";
     apt install -y npm > /dev/null 2>&1;
+    # Check if the last command succeeded
+    if [ $? -eq 0 ]; then
+        echo -e "\e[0;32mSuccess!\e[0m"
+    else
+        echo -e "\e[0;33mFailed! Exit.\e[0m";
+        exit 1;
+    fi
 fi
 
 # Check for PM2. If it's not installed, install it.
-echo -e "\e[0mChecking for Node Process Manager (PM2)...\e[0m"
+echo -n -e "\e[0mChecking for Node Process Manager (PM2) \e[0m"
 if which pm2 >/dev/null 2>&1; then 
-    echo -e "\e[0;32mPM2 is installed \e[0m"; 
+    echo -e "\e[0;32mInstalled \e[0m"; 
 else 
-    echo -e "\e[0;33mPM2 is not installed \e[0m";
-    echo -e "\e[0mInstalling PM2 using npm \e[0m";
+    echo -e "\e[0;33mNot installed \e[0m";
+    echo -n -e "\e[0mInstalling PM2 using npm \e[0m";
     npm install -g pm2 > /dev/null 2>&1;
-    echo -e "\e[0mMaking sure PM2 runs on boot \e[0m";
-    pm2 startup systemd 
+    # Check if the last command succeeded
+    if [ $? -eq 0 ]; then
+        echo -e "\e[0;32mSuccess!\e[0m"
+        echo -e "\e[0mMaking sure PM2 runs on boot \e[0m";
+        pm2 startup systemd 
+    else
+        echo -e "\e[0;33mFailed! Exit.\e[0m";
+    fi
 fi
 
 # Check for CMAKE (required by AWS SDK). If it's not installed,
 # install it.
-echo -e "\e[0mChecking for cmake (required by AWS SDK)...\e[0m"
+echo -n -e "\e[0mChecking for cmake \e[0m"
 if which cmake >/dev/null 2>&1; then  
-    echo -e "\e[0;32mcmake is installed \e[0m"; 
+    echo -e "\e[0;32mInstalled \e[0m"; 
 else 
-    echo -e "\e[0;33mcmake is not installed \e[0m";
-    echo -e "\e[0mInstalling cmake using apt \e[0m";
+    echo -e "\e[0;33mNot installed \e[0m";
+    echo -n -e "\e[0mInstalling cmake using apt \e[0m";
     apt install -y cmake > /dev/null 2>&1
+    # Check if the last command succeeded
+    if [ $? -eq 0 ]; then
+        echo -e "\e[0;32mSuccess!\e[0m"
+    else
+        echo -e "\e[0;33mFailed! Exit.\e[0m";
+        exit 1;
+    fi
 fi
 
 # Check for JQ (required by this script). If it's not installed,
 # install it.
-echo -e "\e[0mChecking for jq (required by installer)...\e[0m"
-if which cmake >/dev/null 2>&1; then  
-    echo -e "\e[0;32mjq is installed \e[0m"; 
+echo -n -e "\e[0mChecking for jq \e[0m"
+if which jq >/dev/null 2>&1; then  
+    echo -e "\e[0;32mInstalled \e[0m"; 
 else 
-    echo -e "\e[0;33mjq is not installed \e[0m";
-    echo -e "\e[0mInstalling jq using apt \e[0m";
+    echo -e "\e[0;33mNot installed \e[0m";
+    echo -n -e "\e[0mInstalling jq using apt \e[0m";
     apt install -y jq > /dev/null 2>&1
+    # Check if the last command succeeded
+    if [ $? -eq 0 ]; then
+        echo -e "\e[0;32mSuccess!\e[0m"
+    else
+        echo -e "\e[0;33mFailed! Exit.\e[0m";
+        exit 1;
+    fi
 fi
 
 
@@ -83,32 +123,54 @@ fi
 
 # Check for the latest release of the EdgeBerry application using the
 # GitHub API
-echo -e "\e[0mGetting latest release of the ${APPNAME} Application \e[0m"
+echo -n -e "\e[0mGetting latest ${APPNAME} release \e[0m"
 latest_release=$(curl -H "Authorization: token ${ACCESSTOKEN}" -H "Accept: application/vnd.github.v3+json" -s "https://api.github.com/repos/${REPOOWNER}/${REPONAME}/releases/latest")
 asset_url=$(echo "$latest_release" | jq -r '.assets[] | select(.name | test("EdgeBerry-v[0-9]+\\.[0-9]+\\.[0-9]+\\.tar\\.gz")) | .url')
-echo -e "\e[0;32mGot download URL:\e[0m ${asset_url}"; 
 # If we have an asset URL, download the tarball
 if [ -n "$asset_url" ]; then
-    echo -e "\e[0mDownloading the application...\e[0m"
+    #echo -e "\e[0;32mURL:\e[0m ${asset_url}";
+    echo -e "\e[0;32mSuccess!\e[0m"; 
+    echo -n -e "\e[0mDownloading the application \e[0m"
     curl -L \
     -H "Accept: application/octet-stream" \
     -H "Authorization: Bearer ${ACCESSTOKEN}" \
     -H "X-GitHub-Api-Version: 2022-11-28" \
     -o "repo.tar.gz" \
     "$asset_url" > /dev/null 2>&1
+    # Check if the download was successful
+    if [ $? -eq 0 ]; then
+        echo -e "\e[0;32mSuccess!\e[0m"
+    else
+        echo -e "\e[0;33mFailed! Exit.\e[0m";
+        exit 1;
+    fi
 else
-    echo -e "\e[0;33mFailed to get the latest release URL\e[0m";
+    echo -e "\e[0;33mFailed! Exit.\e[0m";
     exit 1;
 fi
 
 #Untar the application in the application folder
-echo -e "\e[0mUnpacking the application in the application folder...\e[0m"
-mkdir /opt/${APPNAME}
+echo -n -e "\e[0mUnpacking the application \e[0m"
+mkdir /opt/${APPNAME}  > /dev/null 2>&1;
 tar -xvzf repo.tar.gz -C /opt/${APPNAME} > /dev/null 2>&1
+# Check if the last command succeeded
+if [ $? -eq 0 ]; then
+    echo -e "\e[0;32mSuccess!\e[0m"
+else
+    echo -e "\e[0;33mFailed! Exit.\e[0m";
+    exit 1;
+fi
 
 # Install package dependencies
-echo -e "\e[0mInstalling dependencies...\e[0m"
+echo -n -e "\e[0mInstalling dependencies \e[0m"
 npm install --prefix /opt/${APPNAME} > /dev/null 2>&1
+# Check if the last command succeeded
+if [ $? -eq 0 ]; then
+    echo -e "\e[0;32mSuccess!\e[0m"
+else
+    echo -e "\e[0;33mFailed! Exit.\e[0m";
+    exit 1;
+fi
 
 # Cleanup the download
 rm -rf repo.tar.gz
@@ -118,27 +180,42 @@ rm -rf repo.tar.gz
 ##
 
 # Get the latest release of the UI
-echo -e "\e[0mGetting latest release of the ${APPNAME} UI \e[0m"
+echo -n -e "\e[0mGetting latest ${APPNAME} UI release \e[0m"
 latest_release=$(curl -H "Authorization: token ${ACCESSTOKEN}" -H "Accept: application/vnd.github.v3+json" -s "https://api.github.com/repos/${REPOOWNER}/${REPONAME}-ui/releases/latest")
 asset_url=$(echo "$latest_release" | jq -r '.assets[] | select(.name | test("EdgeBerry-UI-v[0-9]+\\.[0-9]+\\.[0-9]+\\.tar\\.gz")) | .url')
-echo -e "\e[0;32mGot download URL:\e[0m ${asset_url}"; 
 # If we have an asset URL, download the tarball
 if [ -n "$asset_url" ]; then
-    echo -e "\e[0mDownloading the user interface...\e[0m"
+    #echo -e "\e[0;32mGot URL:\e[0m ${asset_url}"; 
+    echo -e "\e[0;32mSuccess!\e[0m"; 
+    echo -n -e "\e[0mDownloading the latest ${APPNAME} UI \e[0m"
     curl -L \
     -H "Accept: application/octet-stream" \
     -H "Authorization: Bearer ${ACCESSTOKEN}" \
     -H "X-GitHub-Api-Version: 2022-11-28" \
     -o "ui.tar.gz" \
     "$asset_url" > /dev/null 2>&1
+    # Check if the download was successful
+    if [ $? -eq 0 ]; then
+        echo -e "\e[0;32mSuccess!\e[0m"
+    else
+        echo -e "\e[0;33mFailed! Exit.\e[0m";
+        exit 1;
+    fi
 else
-    echo -e "\e[0;33mFailed to get the latest UI release URL\e[0m";
+    echo -e "\e[0;33mFailed! Exit.\e[0m";
     exit 1;
 fi
 
 # Unpack the UI in the build/public folder of the application
-echo -e "\e[0mUnpacking the application in the application UI folder...\e[0m"
+echo -n -e "\e[0mUnpacking the application \e[0m"
 tar -xvzf ui.tar.gz -C /opt/${APPNAME}/build/public > /dev/null 2>&1
+# Check if the last command succeeded
+if [ $? -eq 0 ]; then
+    echo -e "\e[0;32mSuccess!\e[0m"
+else
+    echo -e "\e[0;33mFailed! Exit.\e[0m";
+    exit 1;
+fi
 
 # Cleanup the download
 rm -rf ui.tar.gz
@@ -148,13 +225,39 @@ rm -rf ui.tar.gz
 ##
 
 # Start the application using PM2
+echo -n -e "\e[0mStarting the ${APPNAME} Application using PM2 \e[0m"
 cd /opt/${APPNAME}
 pm2 start npm --name ${APPNAME} -- start > /dev/null 2>&1
+# Check if the last command succeeded
+if [ $? -eq 0 ]; then
+    echo -e "\e[0;32mSuccess!\e[0m"
+else
+    echo -e "\e[0;33mFailed! Exit.\e[0m";
+    exit 1;
+fi
 
 # Save the PM2 state, so the application will automatically
 # run after a reboot
+echo -n -e "\e[0mSaving the current PM2 state \e[0m"
 pm2 save > /dev/null 2>&1
+# Check if the last command succeeded
+if [ $? -eq 0 ]; then
+    echo -e "\e[0;32mSuccess!\e[0m"
+else
+    echo -e "\e[0;33mFailed! Exit.\e[0m";
+    exit 1;
+fi
 
-# Exit succes
-echo -e "\e[0;32m${APPNAME} is installed! \e[0m"; 
+# Exit success
+echo ""
+echo -e "\e[0;32m\033[1m${APPNAME} was successfully installed! \033[0m\e[0m"; 
+echo ""
+
+ip_address=$(awk '/inet / && $2 != "127.0.0.1"{print $2}' <(ifconfig))
+echo -e "Access the \033[1m${APPNAME} web interface\033[0m on your local network by"
+echo -e "opening a browser and going to \033[4mhttp://${ip_address}:3000\033[0m"
+echo ""
+
+
+# Exit success
 exit 0;
